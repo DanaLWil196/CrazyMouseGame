a946601de3524bb7fba47ed26d4a52fa7776add5
Dana Wilson <danalwil@outlook.com>
2020-10-27 13:47:52 -0400

    Updated with Timer

--- CrazyMouseGame/MainWindow.xaml.cs
+++ CrazyMouseGame/MainWindow.xaml.cs
@@ -19,18 +19,29 @@ using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace CrazyMouseGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public static Random _random = new Random();
+        private int Score = 0;
+        private int PreviousScore = 0;
         public static int xMouseLocation = 20;
         public static int yMouseLocation = 20;
+        DispatcherTimer _timer;
+        TimeSpan _time;
 
         public MainWindow()
         {
             InitializeComponent();
+            //Tet should show High Score that has been made by a player for the new player's to challenge and beat//
+            PastScore.Text = "The previous score is {PreviousScore} by {}.";
+            _time = TimeSpan.FromSeconds(90);
+            _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                Timer.Text = _time.ToString("c");
+                if (_time == TimeSpan.Zero) _timer.Stop();
+                _time = _time.Add(TimeSpan.FromSeconds(-1));
+            }, System.Windows.Application.Current.Dispatcher);
+            _timer.Start();
             Thread crazyMouseThread = new Thread(new ThreadStart(CrazyMouseThread));
             crazyMouseThread.Start();
         }
@@ -39,35 +50,36 @@ namespace CrazyMouseGame
         {
             int moveX = 0;
             int moveY = 0;
-
             while (true)
             {
                 moveX = _random.Next(xMouseLocation) - (xMouseLocation / 2);
                 moveY = _random.Next(yMouseLocation) - (yMouseLocation / 2);
-
                 System.Windows.Forms.Cursor.Position = new System.Drawing.Point(System.Windows.Forms.Cursor.Position.X + moveX, System.Windows.Forms.Cursor.Position.Y + moveY);
                 Thread.Sleep(50);
             }
         }
 
-        private int Score = 0;
-
         private void mousebtn_Click(object sender, RoutedEventArgs e)
         {
             Score++;
             score.Text = Score.ToString();
-
             if (mousebtn.Height <= 3)
             {
                 MessageBox.Show("Congrats You Win!");
-
-                //Environment.Exit(0);
+                if (Score > PreviousScore)
+                {
+                    //needs to record score that has been made, along with player's name and both added to file//
+                }
+                else
+                {
+                    MessageBox.Show("Better Luck Next Time on getting the High Score.");
+                    Environment.Exit(0);
+                }
             }
             else
             {
                 xMouseLocation = xMouseLocation + 2;
                 yMouseLocation = yMouseLocation + 2;
-
                 mousebtn.Height = mousebtn.Height - 5;
                 mousebtn.Width = mousebtn.Width - 7;
             }
